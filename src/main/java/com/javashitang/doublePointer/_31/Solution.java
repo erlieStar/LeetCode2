372f621330e4901fcdce1eecaf0c9b19db0aa061
lilimin <290059742@qq.com>
2021-06-30 17:30:09 +0800

    增加双指针题目

---
+++ src/main/java/com/javashitang/doublePointer/_31/Solution.java
@@ -0,0 +1,18 @@
+package com.javashitang.doublePointer._31;
+
+/**
+ * @author lilimin
+ * @since 2021-06-30
+ * 1 1 2
+ * 1 2 1
+ *
+ * 2 3 4
+ * 3 2 4
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int left = nums.length - 1;
+        int right = nums.length - 1;
+
+    }
+}
