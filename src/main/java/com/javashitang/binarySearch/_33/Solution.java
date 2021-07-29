8734993045fc7bbd640553b1bc804a4405c9cfa9
lilimin <290059742@qq.com>
2021-07-29 23:44:11 +0800

    增加部分题解

--- src/main/java/com/javashitang/binarySearch/_33/Solution.java
+++ src/main/java/com/javashitang/binarySearch/_33/Solution.java
@@ -6,9 +6,12 @@ package com.javashitang.binarySearch._33;
  * https://blog.csdn.net/zhangbaoanhadoop/article/details/80036931
  */
 class Solution {
+
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        return 0;
+    }
+
+    public int binarySearch(int[] nums, int target, int left, int right) {
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
