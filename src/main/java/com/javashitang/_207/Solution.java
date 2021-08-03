00be2c5572392a3bc9735f691064789df5d11996
lilimin <290059742@qq.com>
2021-08-03 19:28:06 +0800

    增加动态规划的题解

---
+++ src/main/java/com/javashitang/_207/Solution.java
@@ -0,0 +1,11 @@
+package com.javashitang._207;
+
+/**
+ * @author lilimin
+ * @since 2021-08-03
+ */
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        return true;
+    }
+}
