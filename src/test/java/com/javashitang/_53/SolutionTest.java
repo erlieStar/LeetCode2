2d25f8f1f57bec47ee84b84e60251a08c1c373d3
lilimin <290059742@qq.com>
2021-08-31 11:06:17 +0800

    增加demo

--- src/test/java/com/javashitang/_53/SolutionTest.java
+++ src/test/java/com/javashitang/_53/SolutionTest.java
@@ -2,9 +2,8 @@ package com.javashitang._53;
 
 import org.junit.Test;
 
-import java.net.Socket;
-
-import static org.junit.Assert.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * @author lilimin
@@ -18,5 +17,35 @@ public class SolutionTest {
         Solution solution = new Solution();
         int i = solution.maxSubArray(array);
         System.out.println(i);
+    }
+
+    public class Student {
+        private int age;
+        private String name;
+        public Student(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "age=" + age +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    @Test
+    public void test() {
+        Queue<Student> students = new PriorityQueue<>((t1, t2) -> t1.age - t2.age);
+        students.add(new Student(2, "1"));
+        students.add(new Student(1, "1"));
+        students.add(new Student(1, "2"));
+        students.add(new Student(1, "3"));
+        while (!students.isEmpty()) {
+            System.out.println(students.poll());
+        }
+
     }
 }
